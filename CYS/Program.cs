d88e91eeabb8303a0e955c27577e35e635cba577
mhmmdmngl <sorusora42@gmail.com>
2024-09-11 04:23:28 +0300

    1

--- CYS/Program.cs
+++ CYS/Program.cs
@@ -17,7 +17,7 @@ if (!app.Environment.IsDevelopment())
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
-//builder.WebHost.UseUrls("http://0.0.0.0:80"); // veya 80 portunu kullanýyorsanýz "http://0.0.0.0:80"
+builder.WebHost.UseUrls("http://0.0.0.0:80"); // veya 80 portunu kullanýyorsanýz "http://0.0.0.0:80"
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
