90885b39a6eb03a39cf44304eedae738311c527e
mhmmdmngl <sorusora42@gmail.com>
2024-10-07 01:48:58 +0300

    s

--- CYS/Program.cs
+++ CYS/Program.cs
@@ -20,17 +20,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name v1"));
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name v1"));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
